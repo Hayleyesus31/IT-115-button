6d482f5079245612d97ebec3e2e2506853d469b7
Hayleyesus Gujo <12hayleloke@gmail.com>
2023-05-28 16:29:55 -0700

    add comment

--- Button.py
+++ Button.py
@@ -1,17 +1,17 @@
-# 
+# import tkinter library
 import tkinter as tk
 
-#
+#  define a function that is exucuted when button is clicked.
 def button_click():
     print("Button clicked!")
     
-#
+#Create the parent window of the application.
 root = tk.Tk()
 root.title("Button Example")
 
-#
+# create the button object and assign the button_click() function as the command. 
 button = tk.Button(root,text="Click Me!", command=button_click)
 button.pack()
 
-#
+# start the event loop to run the application.
 root.mainloop()
